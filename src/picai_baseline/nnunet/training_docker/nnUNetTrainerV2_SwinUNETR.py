f3787b26e84874af1df35f6433302031e19fe87e
Joeran Bosma <joeran@bosma.co>
2022-07-04 17:01:21 +0200

    Next

--- src/picai_baseline/nnunet/training_docker/nnUNetTrainerV2_SwinUNETR.py
+++ src/picai_baseline/nnunet/training_docker/nnUNetTrainerV2_SwinUNETR.py
@@ -10,10 +10,12 @@ class nnUNetTrainerV2_SwinUNETR(nnUNetTrainerV2_Loss_FL_and_CE_checkpoints):
                  unpack_data=True, deterministic=True, fp16=False):
         super().__init__(plans_file, fold, output_folder, dataset_directory, batch_dice, stage, unpack_data,
                          deterministic, fp16)
+        print("Hello here.")
+        print(self.was_initialized)
 
     def initialize_network(self):
         """Initialize SwinUNETR network"""
-        
+
         print("Hello there!")
         print("="*100)
         print(self.network)
